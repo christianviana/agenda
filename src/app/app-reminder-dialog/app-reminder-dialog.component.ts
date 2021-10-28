0c1d578950bbb18d745d60c92f56a91aef2e9a0c
Christian Lopes Viana <christlv@trt3.jus.br>
2021-10-28 18:25:25 -0300

    limpeza

--- src/app/app-reminder-dialog/app-reminder-dialog.component.ts
+++ src/app/app-reminder-dialog/app-reminder-dialog.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnInit, ViewChild} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { Reminder } from '../model/Reminder';
